2c4a999fcf589b7c8d53c50bf217be54efeadc3a
cukie <gil.cukierman@gmail.com>
2015-04-29 09:40:57 -0400

    added playground for testing modules

---
+++ test.py
@@ -0,0 +1,11 @@
+# Author: Gil Cukierman
+# gil.cukierman (at) gmail.com
+# Creation Date: 4/29/15
+# (c) All Rights Reserved
+
+from gui import getParentFile
+# a place to test our modules out!
+
+####### gui.getParentFile() tester #############
+filename = getParentFile()
+print filename
