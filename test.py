68ce757d8479d56ac016e31ff7c00e54c1feb679
cukie <gil.cukierman@gmail.com>
2015-04-29 10:23:41 -0400

    tester for getImage()

--- test.py
+++ test.py
@@ -3,9 +3,16 @@
 # Creation Date: 4/29/15
 # (c) All Rights Reserved
 
+from __future__ import print_function
 from gui import getParentFile
+import images as images
 # a place to test our modules out!
 
 ####### gui.getParentFile() tester #############
 filename = getParentFile()
-print filename
+print (filename)
+
+
+####### images.getImage tester #################
+im = images.getImage("/Users/cukierma/Desktop/APIKEY_amw.tiff")
+print(im.format, im.size, im.mode) # see what we got
