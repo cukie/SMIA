86d77250a3017359c3a96d987ed492f00395ed99
cukie <gil.cukierman@gmail.com>
2015-04-29 17:11:02 -0400

    functionality to list directory contents and iterate through them

--- images.py
+++ images.py
@@ -4,7 +4,14 @@
 # (c) All Rights Reserved
 
 from PIL import Image
+import os
 
+
+# listdir wrapper that returns the full path of children based on root
+def listdir_fullpath(d):
+    return [os.path.join(d, f) for f in os.listdir(d)]
+
+# returns an image object from the given filepath
 def getImage(filepath):
 	im = Image.open(filepath)
 
--- test.py
+++ test.py
@@ -12,6 +12,11 @@ import images as images
 filename = getParentFile()
 print (filename)
 
+####### listdir_fullpath(d) tester iterate through parent directory tester ########
+for directory in images.listdir_fullpath(filename):
+	print (directory," ------------ ")
+	for pic in images.listdir_fullpath(directory):
+		print (pic) 
 
 ####### images.getImage tester #################
 im = images.getImage("/Users/cukierma/Desktop/APIKEY_amw.tiff")
