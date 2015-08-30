58833531b070cfba6444be8764e96a4fae7e4bf4
cukie <gil.cukierman@gmail.com>
2015-08-30 11:02:49 -0400

    BatchRunner created
    The LoopDirectory logic has now been refactored into BatchRunner… This
    has not yet been tested, just a check in to mark progress.

--- SMIA-CUKIE/images2.py
+++ SMIA-CUKIE/images2.py
@@ -3,7 +3,7 @@
 # @Author: cukierma
 # @Date:   2015-08-30 09:19:30
 # @Last Modified by:   cukie
-# @Last Modified time: 2015-08-30 10:03:26
+# @Last Modified time: 2015-08-30 10:57:18
 
 # NOTE: This is just a working copy while we do our refactoring
 
@@ -51,6 +51,8 @@ class BatchRunner():
 		self.output_images = output_images
 		self.output_thumbnails = output_thumbnails
 		self.output_path = output_path
+		self.first_dir = True # Helps us in determining when to write our headers in our results.
+		self.currentBatch = None # We need a reference to the current BatchImage Object we are processing for, thus, we save it here.
 
 	def run(self):
 		'''
@@ -59,17 +61,96 @@ class BatchRunner():
 
 		# The main flow here is to loop through each directory creating a list of masks and marker objects respectively.
 		# We then create our BatchImage object, perform operations, and write results
-		masks, markers = self._masksAndMarkersFromTopDir()
-		batch = BI.BatchImage(
-			masks,
-			markers,
-			self.num_layers,
-			self.white_list,
-			makeimages=self.output_images,
-			makethumbnails=self.output_thumbnails
-			)
+		for directory in self._listdir_fullpath(self.base_dir):
+			masks, markers = self._masksAndMarkersFromTopDir()
+			batch = BI.BatchImage(
+				masks,
+				markers,
+				self.num_layers,
+				self.white_list,
+				makeimages=self.output_images,
+				makethumbnails=self.output_thumbnails
+				)
 
-	def _masksAndMarkersFromTopDir(self):
+			# Give the object a reference to our current batch
+			self.currentBatch = batch
+
+			# now we performOps on our current BatchImage instance and save the results.
+			results = self._runOperations()
+			self._saveResults(results, directory) 
+
+	def _runOperations(self, batch):
+		'''
+		Takes a batch and calls performOps on it, aggregating results as we go in an OrderedDict.
+
+		:param BatchImage batch: The batch we want to run
+		:return OrderedDict: An OrderedDict of resutls.
+
+		'''
+
+		output_dict = OrderedDict()
+		# make sure we always have a directory name
+		output_dict['Directory Name'] = directory
+
+		for results in self.currentBatch.performOps():
+			output_dict = self._mergeDicts(output_dict,results)
+
+		return output_dict
+
+	def _saveResults(self, results_dict, current_directory):
+		'''
+		We save results based on the output parameters passed into our BatchRunner instance
+		'''
+		# open our results file
+		f = open(os.path.join(self.output_path,"results.csv"),'wb')
+		writer = None
+
+		if self.first_dir:
+			fieldnames = list(results_dict.keys())
+			writer = csv.DictWriter(f, fieldnames=fieldnames,dialect='excel')
+			writer.writeheader()
+
+		writer.writerow(output_dict)
+		del output_dict # TODO: Is this needed? Holdover from memory leak search?
+
+		if self.output_images:
+			# Create a folder for each batch for image results
+
+			save_location = os.path.join(self.output_path, ntpath.basename(current_directory) + " images")
+			os.makedirs(save_location)
+
+			for image in self.currentBatch.AllResultImages():
+				img = image[0]
+				name = image[1]
+
+				img.save(os.path.join(save_location, name + '.tif'))
+				img.close()
+				
+		if output_thumbnails:
+			# Create a folder for each batch for image results
+
+			save_location = os.path.join(self.output_path, ntpath.basename(directory) + " thumbnails")
+			os.makedirs(save_location)
+
+			for image in self.currentBatch.AllResultThumbnails():
+				img = image[0]
+				name = image[1]
+				img.save(os.path.join(save_location, name + '.jpg'))
+				img.close()
+
+		f.close()
+
+	def mergeDicts(self,dict1,dict2):
+		"""
+		Takes two dictionaries and merges them.
+		"""
+
+		x = dict1.copy()
+		x.update(dict2)
+
+		return x
+
+	def _masksAndMarkersFromDir(self, dir):
 		'''
 		This helper function loops through our images and creates a list of masks and markers
 		in preparation for creating a BatchImage object.
@@ -77,19 +158,18 @@ class BatchRunner():
 		masks = []
 		markers = []
 
-		for directory in self._listdir_fullpath(self.base_dir):
-			for pic in self._listdir_fullpath(directory):
-				picType, picObj = self._createSinglePicObj(pic, withNegative=True)
+		for pic in self._listdir_fullpath(directory):
+			picType, picObj = self._createSinglePicObj(pic, withNegative=True)
 
-				if picType == 'mask':
-					masks.append(picObj[1])
-					masks.append(picOBj[2])
-				if picType == 'marker':
-					markers.append(picObj[1])
-					markers.append(picObj[2])
-				#TODO: right now we are failing silently here...
+			if 'mask' == picType:
+				masks.append(picObj[1])
+				masks.append(picOBj[2])
+			if 'marker' == picType:
+				markers.append(picObj[1])
+				markers.append(picObj[2])
+			#TODO: right now we are failing silently here...
 
-		return maks, markers 
+		return masks, markers 
 
 
 	def _listdir_fullpath(self, d):
